f7e37c32559135cdf885b45093ef34d36e57f1b0
StacyDu <dudinaanastasiia@gmail.com>
2019-07-30 20:15:59 +0200

    Complete homework

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,23 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+const list = document.querySelector('.list--js');
+
+fetch('https://api.github.com/users/stacydu/repos?sort=update&direction=desc')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos =resp;
+    for (const repo of repos) {
+      const {html_url, name} = repo;
+      list.innerHTML += `
+      <li class='list__item'>
+        <a class='list__link' href='${html_url}'>${name}</a>
+      </li>
+      `;
+    }
+  })
+  .catch (err => {
+    console.log(err);
+  })
 
 
